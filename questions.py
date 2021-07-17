6340656af285686fc88edd7d783ffa137dfc6d9b
Efosa Asemota <efosa.asemota@morehouse.edu>
2021-07-17 19:13:32 -0400

    created new questions

---
+++ questions.py
@@ -0,0 +1,57 @@
+class Questions:
+    
+    def __init__(self):
+        
+        quiz = {
+            1 : {
+                "question" : "After you stop and drop, what should you do next?",
+                "answers" : ["shut 'em down, open up shop", "put your hands up", "roll", "hand over your liscense and registration"],
+                "answer" : "shut 'em down, open up shop"
+                
+            },
+            2 : {
+                "question" : "If Young Metro don't trust you, what might Future do?" ,
+                "answers" : ["call the police", "shoot you", "fight you", "beat you"],
+                "answer" : "shoot you"
+            },
+            3 : {
+                "question" : "What might momma tell you before going into any store?",
+                "answers" : ["don't touch nothing", "don't ask for nothing", "don't look for nothing", "all of the above"],
+                "answer" : "all of the above"
+            },
+            4 : {
+                "question" : "Who was Oprah Winfrey's best friend?",
+                "answers" : ["gayle king", "tyler perry", "stedman graham", "cicely tyson"],
+                "answer" : "gayle king"
+            },
+            5 : {
+                "question" : "In what city did Rosa Parks refuse to give up her seat?",
+                "answers" : ["tuskegee, alabama", "montgomery, alabama", "birmingham, alabama", "jackson, mississippi"],
+                "answer" : "montgomery, alabama"
+            },
+            6 : {
+                "question" : "According to Kanye, who doesn't have the answers?",
+                "answers" : ["taylor swift", "beck", "jay-z", "sway"],
+                "answer" : "sway"
+            },
+            7 : {
+                "question" : "In what years did Cash Money Records take over?" ,
+                "answers" : ["2001 - 2002", "1999 - 2000", "1997 - 1998", "2004 - 2005"],
+                "answer" : "1999 - 2000"
+            },
+            8 : {
+                "question" : "In what city was MLK Jr Killed?",
+                "answers" : ["atlanta, ga", "washington dc", "birmingham, al" "memphis, tn"],
+                "answer" "memphis, tn":
+            },
+            9 : {
+                "question" : "Who is Steve Urkel's cool alter ego?",
+                "answers" : ["stan", "scotty", "stefan", "shawn"],
+                "answer" : "stefan"
+            },
+            10 : {
+                "question" : "What might momma say you need in order to get some McDonald's?",
+                "answers" : ["a job", "a clue", "good grades", "mcdonald's money"],
+                "answer" : "mcdonald's money"
+            }
+        }
