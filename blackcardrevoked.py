acc380faba5b2414de17c6fa3c09c9b182faf7a2
Prince Ndhlovu <princendhlovu@Princes-Air.attlocal.net>
2021-07-21 11:48:43 -0500

    added methods to the classes

---
+++ blackcardrevoked.py
@@ -0,0 +1,30 @@
+from Black-Card-Revoked import questions.py
+
+class BlackCardRevoked:
+
+	def __init__(self):
+
+		player = "--"
+		points = {} 	#key = player_name, value = player_points
+		answer = " "
+
+	#takes user input
+	def takeInput(self):
+
+	#adds or deducts points after each question or round
+	def scorePoints(self):
+
+	#who is replying now and next
+	def repliesFirst(self):
+
+	#checks to see which player has the most points at the end of the game
+	def checkWinner(self):
+
+	#This function will check the user’s input to see if it is a correct answer
+	def checkInput(self):
+
+	#this function initializes the game, lays out the rules for the game as well as serving as the main loop of the game.
+	def playGame(self):
+
+
+
